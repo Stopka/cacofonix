ad1ae75ee48aaa3a70cd27bef7c009a1906b5b78
Štěpán Škorpil <stepan@skorpil.cz>
2023-12-07 00:26:36 +0100

    Fixed toggle

--- build/frontend/application/src/components/client/NavigationToggler.tsx
+++ build/frontend/application/src/components/client/NavigationToggler.tsx
@@ -1,15 +1,28 @@
 'use client'
-import { ReactElement, ReactNode, useState } from 'react'
+import { ReactElement, ReactNode, useEffect, useRef, useState } from 'react'
 import Icon from '../server/Icon'
 
 export default function NavigationToggler ({ children }: { children: ReactNode }): ReactElement {
   const [isOpen, setOpen] = useState<boolean>(false)
+  const btnRef = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    const onClickOutside = (event): void => {
+      if (btnRef?.current?.contains(event.target) === false) {
+        setOpen(false)
+      }
+    }
+    if (isOpen) {
+      document.addEventListener('mousedown', onClickOutside)
+    } else {
+      document.removeEventListener('mousedown', onClickOutside)
+    }
+  }, [isOpen])
   return <>
-        <button aria-controls="basic-navbar-nav" type="button" aria-label="Přepínač navigace"
+        <button ref={btnRef} aria-controls="basic-navbar-nav" type="button" aria-label="Přepínač navigace"
                 className={'navbar-toggler ' + (isOpen ? '' : 'collapsed')} onClick={() => setOpen(!isOpen)}>
             <Icon iconName={isOpen ? 'menu_open' : 'menu'} />
         </button>
-        <div className={'navbar-collapse collapse' + (isOpen ? 'show' : '')} id="basic-navbar-nav">
+        <div className={'navbar-collapse collapse' + (isOpen ? ' show' : '')} id="basic-navbar-nav">
             <div className="navbar-nav me-auto nav">
                 {children}
             </div>
